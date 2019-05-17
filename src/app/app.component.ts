c6085f1609e32caeabf2b426e291435ae3a162e9
Jose Miguel Gómez Lozano <josemygel@gmail.com>
2019-05-17 10:28:53 +0200

    fixed fail import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,6 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Storage } from '@ionic/storage';
-import { Receta } from './receta';
-import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-root',
