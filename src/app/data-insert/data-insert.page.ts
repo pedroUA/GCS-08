7ec0bc120795456f4d3858fa705baaf3c5b8440f
Javier Agustín Tyszko González <javitg96@gmail.com>
2019-05-17 21:04:33 +0200

    Data Insert Page

--- src/app/data-insert/data-insert.page.ts
+++ src/app/data-insert/data-insert.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
+import {FormGroup, FormBuilder, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-data-insert',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataInsertPage implements OnInit {
 
-  constructor() { }
+  typeForm = 'peso';
+  formpeso: FormGroup;
+  formcalorico: FormGroup;
+
+  constructor(private storage: Storage, private router: Router, public formBuilder: FormBuilder) {
+      this.formpeso = new FormGroup({
+        peso: new FormControl,
+        altura: new FormControl
+      });
+
+      this.formcalorico = new FormGroup({
+        grasas: new FormControl,
+        hidratos: new FormControl,
+        proteinas: new FormControl
+      });
+  }
 
   ngOnInit() {
+      //al iniciar
   }
+
+  changeForm() {
+      const pesoButtonActive = document.getElementById('peso-button').classList.contains('active');
+      const caloricoButtonActive = document.getElementById('calorico-button').classList.contains('active');
+      if (pesoButtonActive) {
+          //desactivar boton peso
+          //desactivar form peso
+          //activar boton calorico
+          //activar form calorico
+      } else {
+          //al contrario
+      }
+  }
+
+  changeClassActive(item) {
+      if (!document.getElementById(item).classList.contains('active')) {
+          document.getElementById('recetas-button').classList.toggle('active');
+          document.getElementById('usuarios-button').classList.toggle('active');
+
+          this.changeForm();
+      }
+ }
 
 }
